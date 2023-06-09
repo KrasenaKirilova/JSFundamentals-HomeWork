ce9417155e1534a9573c0a9a2abf30678f0110c9
KrasenaKirilova <krasena.kirilova@gmail.com>
2023-06-09 08:45:56 +0300

    Update rotateArray.js

--- 26_05_23/rotateArray.js
+++ 26_05_23/rotateArray.js
@@ -1,8 +1,19 @@
-function rotateArray (arr) {
+function rotateArray(arr) {
+    let rotationCount = arr[arr.length - 1];
+    let lastElement = arr[arr.length - 2];
 
-    let element = Number(arr.lenght - 1);
-    arr.unshift(element);
-    console.log(arr);
+    for (let i = 0; i < rotationCount; i++) {
+
+        arr.unshift(lastElement);
+        arr.splice(lastElement, 2);
+      
+        
+
+    }
+
+    console.log(arr.join(' '));
 }
 
 rotateArray(['1', '2', '3', '4', '2']);
+// rotateArray(['Banana', 'Orange', 'Coconut',
+// 'Apple', '15']);
