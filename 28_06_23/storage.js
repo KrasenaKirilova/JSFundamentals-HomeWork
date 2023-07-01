0cf941ea62538d6529be36104f994ca49dee93b6
KrasenaKirilova <krasena.kirilova@gmail.com>
2023-07-01 18:48:58 +0300

    add

--- 28_06_23/storage.js
+++ 28_06_23/storage.js
@@ -4,9 +4,16 @@ function storage(input) {
     for (const line of input) {
         let [product, quantity] = line.split(' ');
         storage[product] = Number(quantity);
-        console.log(storage);
-
-
+    }
+    if(!storage.has(product)) {
+        storage.set(product, +quantity);
+    } else {
+        let currentQuantity = storage.get(product);
+        let newQuantity = currentQuantity + quantity;
+        storage.set(product, newQuantity);
+    }
+    for (const [key, value] of storage) {
+        console.log(`${key} -> ${value}`);
     }
 }
 
